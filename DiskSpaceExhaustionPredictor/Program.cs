89c05ba8f281cc44609ecd1dd6523384f7470b61
Ben Hutchison <ben@aldaviva.com>
2019-10-22 07:04:31 -0700

    Initial commit

---
+++ DiskSpaceExhaustionPredictor/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Accord.Statistics.Models.Regression.Linear;
+
+namespace DiskSpaceExhaustionPredictor {
+
+    internal static class Program {
+
+        private static readonly string HISTORY_FILE_PATH =
+            Environment.ExpandEnvironmentVariables(@"%APPDATA%\JAM Software\TreeSize Professional\scanhistory.xml");
+
+        private static int Main(string[] args) {
+            string driveLetter;
+            if (args.Length < 1) {
+                Console.WriteLine($"Usage: {Process.GetCurrentProcess().ProcessName} C:\n" +
+                                  "where C: is the letter of the drive you want to analyze.\n" +
+                                  "Requires a history of TreeScan Professional scans for this drive.");
+                return 1;
+            } else {
+                driveLetter = Path.GetFullPath(args[0]);
+            }
+
+            var deserializer = new XmlSerializer(typeof(ScanHistory));
+            ScanHistory scanHistory;
+            try {
+                using (var fileStream = new FileStream(HISTORY_FILE_PATH, FileMode.Open)) {
+                    scanHistory = (ScanHistory) deserializer.Deserialize(fileStream);
+                }
+            } catch (FileNotFoundException) {
+                Console.WriteLine("Error: TreeSize Professional scan history file was not found while trying to load historical " +
+                                  "records of disk space usage.\n" +
+                                  "Ensure TreeSize Professional 5.2 or similar is installed, " +
+                                  "and than you have run at least two scans at different times.");
+                return 2;
+            }
+
+            IEnumerable<(double dateOA, long allocatedBytes)> matchingRows = (from scan in scanHistory.scans
+                    where Path.GetFullPath(scan.path) == driveLetter
+                    select (dateOA: double.Parse(scan.dateOA), allocatedBytes: long.Parse(scan.sizeData.allocatedBytes)))
+                .ToList();
+
+            switch (matchingRows.Count()) {
+                case 0:
+                    Console.WriteLine(
+                        $"Error: Historical scans were loaded successfully, but drive {driveLetter} has never been scanned.\n" +
+                        $"There were {scanHistory.scans.Count - matchingRows.Count():N0} other, unrelated scan(s) in the TreeSize history.\n" +
+                        "In order to analyze the disk space usage over time, you must scan the disk at least twice.");
+                    return 3;
+                case 1:
+                    Console.WriteLine(
+                        $"Error: Historical scans were loaded successfully, but drive {driveLetter} has only been scanned once.\n" +
+                        $"There were {scanHistory.scans.Count - matchingRows.Count():N0} other, unrelated scan(s) in the TreeSize history.\n" +
+                        "In order to analyze the disk space usage over time, you must scan the disk at least twice.");
+                    return 3;
+                default:
+                    break;
+            }
+
+            double[] dateSequence = matchingRows.Select(x => x.dateOA).ToArray();
+            double[] allocatedByteSequence = matchingRows.Select(x => (double) x.allocatedBytes).ToArray();
+
+            SimpleLinearRegression simpleLinearRegression = SimpleLinearRegression.FromData(dateSequence, allocatedByteSequence);
+            double m = simpleLinearRegression.Slope;
+            double b = simpleLinearRegression.Intercept;
+
+            if (m < 0) {
+                Console.WriteLine($"Disk space usage on {driveLetter} appears to be decreasing over time, " +
+                                  "and will therefore never run out.");
+                return 0;
+            }
+
+            long diskCapacity = new DriveInfo(driveLetter).TotalSize;
+
+            double capacityExhaustionDateExcel = (diskCapacity - b) / m;
+            DateTime capacityExhaustionDate = DateTime.FromOADate(capacityExhaustionDateExcel);
+            double daysRemaining = capacityExhaustionDate.Subtract(DateTime.Now).TotalDays;
+            Console.WriteLine($"Disk space on drive {driveLetter} will be exhausted in about {daysRemaining:N0} days, " +
+                              $"around {capacityExhaustionDate:D}.");
+
+            return 0;
+        }
+
+    }
+
+}
---
+++ DiskSpaceExhaustionPredictor/ScanHistoryData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace DiskSpaceExhaustionPredictor {
+
+    [XmlRoot(ElementName = "SizeData")]
+    public class SizeData {
+
+        [XmlAttribute(AttributeName = "Size")]
+        public string sizeBytes { get; set; }
+
+        [XmlAttribute(AttributeName = "Allocated")]
+        public string allocatedBytes { get; set; }
+
+        [XmlAttribute(AttributeName = "Files")]
+        public string filesCount { get; set; }
+
+    }
+
+    [XmlRoot(ElementName = "Root")]
+    public class Scan {
+
+        [XmlElement(ElementName = "Path")]
+        public string path { get; set; }
+
+        //redundant with child Path element
+//        [XmlAttribute(AttributeName = "path")]
+//        public string path { get; set; }
+
+        [XmlElement(ElementName = "Date")]
+        public string dateOA { get; set; }
+
+        [XmlElement(ElementName = "Version")]
+        public string treeSizeVersion { get; set; }
+
+        [XmlElement(ElementName = "SizeData")]
+        public SizeData sizeData { get; set; }
+
+        [XmlElement(ElementName = "Freespace")]
+        public string freeSpaceBytes { get; set; }
+
+    }
+
+    [XmlRoot(ElementName = "SCANS")]
+    public class ScanHistory {
+
+        [XmlElement(ElementName = "Root")]
+        public List<Scan> scans { get; set; }
+
+    }
+
+}
